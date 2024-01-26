da2c63014d4dad8ccf308be6630ca03be3c94fa6
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2024-01-26 20:18:42 +0000

    Reading File Students

--- src/ca1programming/CA1programming.java
+++ src/ca1programming/CA1programming.java
@@ -4,6 +4,11 @@
  */
 package ca1programming;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 /**
  *
  * @author Capitania
@@ -14,7 +19,13 @@ public class CA1programming {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+ try{
+            BufferedReader br = new BufferedReader( new FileReader("students.txt"));        
 
-    }
     
+    } catch (Exception e) {
+            System.out.println(e);
+    }
+    }
 }
+    
